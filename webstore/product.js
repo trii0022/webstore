1a39de42aa4f711b069bb752e58aa05af3f9a608
Triin Trempi <tri0022@stud.kea.dk>
2021-09-02 10:23:50 +0200

    changes

--- webstore/product.js
+++ webstore/product.js
@@ -1,4 +1,6 @@
-const url = "https://kea-alt-del.dk/t7/api/products/1163";
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = "https://kea-alt-del.dk/t7/api/products/" + id;
 /* const url = "https://kea-alt-del.dk/t7/api/products/2801"; */
 
 // fetch the data
--- webstore/productlist.js
+++ webstore/productlist.js
@@ -1,4 +1,12 @@
-const url = "https://kea-alt-del.dk/t7/api/products/";
+const urlParams = new URLSearchParams(window.location.search);
+const season = urlParams.get("season");
+
+/* const urlParams = new URLSearchParams(window.location.search);
+const price = urlParams.get("price"); */
+
+/* document.querySelector(".price").textContent = price; */
+
+const url = "https://kea-alt-del.dk/t7/api/products?season=" + season;
 
 fetch(url)
   .then(function (res) {
@@ -39,13 +47,24 @@ function showProduct(product) {
   //clone it
 
   const copy = template.cloneNode(true);
+
   //change content
+  copy.querySelector("a").setAttribute("href", "product.html?id=" + product.id);
+
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+
   copy.querySelector(
     ".subtle"
   ).textContent = `${product.articletype}| ${product.brandname}`;
   copy.querySelector("h3").textContent = product.productdisplayname;
 
-  if (product.soldOut) {
+  copy.querySelector(".productname").textContent = product.productdisplayname;
+
+  copy.querySelector(".price").textContent = product.price;
+
+  if (product.soldout) {
     copy.querySelector("article").classList.add("soldOut");
   }
   if (product.discount) {
